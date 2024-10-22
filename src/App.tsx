dc879da06b333c14a841d20cb814f5ea2924c555
nicochnz <chiche.nicolas@gmail.com>
2024-10-22 15:29:18 +0200

    Pokedex-project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import MyTitle from "./components/MyTitle";
+
+
+
+function App() {
+  return (
+    <section>
+      <MyTitle/>
+    </section>
+  );
+}
+
+export default App;
